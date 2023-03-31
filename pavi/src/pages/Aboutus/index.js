3df13bf05c7ca5db599dcea9d1533c7654fb5e5e
Pavithra Kolluri <kolluripavithra02@gmail.com>
2023-03-31 20:22:26 +0530

    pavi

--- pavi/src/pages/Aboutus/index.js
+++ pavi/src/pages/Aboutus/index.js
@@ -1,10 +1,14 @@
-import Basiccard from "../../components/Basiccard";
+import { Typography } from '@mui/material';
 
 export default function Aboutus(){
     return(
         <>
-        This is Aboutus page
-        <Basiccard/>
+<Typography>Name: PAVITHRA</Typography>
+<Typography>Father Name: VEERABABU</Typography>
+<Typography>Mother Name: LAXMI</Typography>
+<Typography>Email: kolluripavi123@gmail.com</Typography>
+<Typography>Phone No.: 9848442277</Typography>
         </>
-    )
-}
+        
+    );
+    }
